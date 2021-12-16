5773019204e694dc5ba60edac72b31430eacb1c9
Ryan Mayberry <ryan@artmail.ca>
2021-12-16 17:55:41 -0600

    sanctum
    use sanctum middleware

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use Tapp\LaravelUppyS3MultipartUpload\Http\Controllers\UppyS3MultipartController;
 
-Route::post('/s3/multipart', [UppyS3MultipartController::class, 'createMultipartUpload']);
+Route::middleware('auth:sanctum')->post('/s3/multipart', [UppyS3MultipartController::class, 'createMultipartUpload']);
 
 Route::options('/s3/multipart', [UppyS3MultipartController::class, 'createMultipartUploadOptions']);
 
-Route::get('/s3/multipart/{uploadId}', [UppyS3MultipartController::class, 'getUploadedParts']);
+Route::middleware('auth:sanctum')->get('/s3/multipart/{uploadId}', [UppyS3MultipartController::class, 'getUploadedParts']);
 
-Route::get('/s3/multipart/{uploadId}/batch', [UppyS3MultipartController::class, 'prepareUploadParts']);
+Route::middleware('auth:sanctum')->get('/s3/multipart/{uploadId}/batch', [UppyS3MultipartController::class, 'prepareUploadParts']);
 
-Route::post('/s3/multipart/{uploadId}/complete', [UppyS3MultipartController::class, 'completeMultipartUpload']);
+Route::middleware('auth:sanctum')->post('/s3/multipart/{uploadId}/complete', [UppyS3MultipartController::class, 'completeMultipartUpload']);
 
-Route::delete('/s3/multipart/{uploadId}', [UppyS3MultipartController::class, 'abortMultipartUpload']);
+Route::middleware('auth:sanctum')->delete('/s3/multipart/{uploadId}', [UppyS3MultipartController::class, 'abortMultipartUpload']);
